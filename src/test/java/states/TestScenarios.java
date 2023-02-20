a928989d9f7e5962e4c774fab94916f65970368a
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-02-20 17:23:01 +0100

    Added tests for LaptimeStopwatch.up()

--- src/test/java/states/TestScenarios.java
+++ src/test/java/states/TestScenarios.java
@@ -71,7 +71,10 @@ public class TestScenarios {
 	  assertSame(LaptimeStopwatch.Instance(), c.currentState);
 	  assertEquals("value of totalTime ", 2, AbstractStopwatch.getTotalTime());
 	  assertEquals("value of lapTime ", 1, AbstractStopwatch.getLapTime());
-	  
+	  c.up(); // record stopwatch laptime
+	  c.tick();
+	  assertSame(RunningStopwatch.Instance(), c.currentState);
+
 	  c.left(); // go back to timer mode (remembering history state)
 	  c.tick();
 	  assertSame(PausedTimer.Instance(), c.currentState);
